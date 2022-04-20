087297ac0e9e3d3ebf6ead225829f36a16a02c3c
Ali <ali@scicoded.com>
2022-04-19 22:40:20 -0500

    commit on "main" and "log_config"

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello AWS")
+from fastapi import FastAPI, HTTPException
+from datetime import datetime, timedelta
+import logging
+from logging.config import dictConfig
+from log_config import log_config
+
+# dictConfig(log_config)
+logger = logging.getLogger("reddit-broker-bot")
+
+app = FastAPI()
+
+
+# class PredictionRequest(BaseModel):
+#     subreddit: str
+
+
+@app.get("/")
+def health():
+    logger.info("Health request received.")
+    return {"Status": "Service is online."}
+
+# from fastapi import FastAPI, File, UploadFile
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
